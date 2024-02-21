c778315c05318e4af666c087cacdac1de14a774b
cj <cjokee@gmail.com>
2024-02-21 06:58:16 +0100

    added threshold so it ignores volume under a given threshold

--- audiocruncher.py
+++ audiocruncher.py
@@ -7,33 +7,34 @@ Author: cjoke
 """
 
 import speech_recognition as sr
+import audioop
 from threading import Thread
 from queue import Queue
 
 
 class SpeechRecognizer:
-    def __init__(self):
+    def __init__(self, threshold=3000):
+        # Initialize the speech recognizer and set the threshold
         self.r = sr.Recognizer()
         self.audio_queue = Queue()
         self.recognize_thread = Thread(target=self.recognize_worker)
         self.recognize_thread.daemon = True
+        self.threshold = threshold
 
     def recognize_worker(self):
+        # This function runs in a separate thread and processes the audio data
         while True:
             audio = self.audio_queue.get()
             if audio is None:
                 break
 
             try:
-                print(
-                    "You said : "
-                    + self.r.recognize_google(audio)
-                )
+                print("You said: " + self.r.recognize_google(audio))
             except sr.UnknownValueError:
-                print("You said ? : ")
+                print("Could not understand audio")
             except sr.RequestError as errormsg:
                 print(
-                    "You need an api_key set up, you gready? ; {0}".format(
+                    "Could not request results from Google Speech Recognition service; {0}".format(
                         errormsg
                     )
                 )
@@ -41,11 +42,16 @@ class SpeechRecognizer:
             self.audio_queue.task_done()
 
     def start(self):
+        # Start the recognition thread and begin recording audio
         self.recognize_thread.start()
-        with sr.Microphone() as source:
+        with sr.Microphone(device_index=None) as source:  # Do not specify a device index
             try:
                 while True:
-                    self.audio_queue.put(self.r.listen(source))
+                    audio = self.r.record(source, duration=5)
+                    rms = audioop.rms(audio.frame_data, audio.sample_width)
+                    # If the audio level is above the threshold, add it to the queue
+                    if rms > self.threshold:
+                        self.audio_queue.put(audio)
             except KeyboardInterrupt:
                 pass
 
